10b09c023cee8e9048f1fccf21ae03b78261293a
BabyBbeam <s5804062636216@email.kmutnb.ac.th>
2021-05-22 19:31:00 +0700

    Test CI/CD

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,10 @@ var expect = require('expect.js')
 
 function add(a, b){ return a & b }
 
+test('renders learn react link', () => {
+  render(<App />);
+});
+
 it('should do math', function (){
   expect(console.log(add(true, true)))
 })
